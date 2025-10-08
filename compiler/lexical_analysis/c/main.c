5d8cac70ab3f1b93acc2e74475215a2616f68e68
PyDevC <anuskcse@gmail.com>
2025-10-08 06:24:18 -0400

    update main.c to read files and stdout

--- compiler/lexical_analysis/c/main.c
+++ compiler/lexical_analysis/c/main.c
@@ -1,12 +1,45 @@
-#include "lexer.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  char *source_code[] = {"local", "a", "=", "\"This is\""};
-  SToken *TokenList = Tokenizer(source_code);
-  int i;
-  for (i = 0; i < 4; i++) {
-    printf("%d ", TokenList[i].Tokentype);
+#define BUFSIZE 1024
+
+static int readable(const char *filename) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL)
+    return 0;
+  fclose(file);
+  return 1;
+}
+
+typedef struct ReaderF {
+  unsigned n; // linenumber
+  unsigned c; // colnumber
+  FILE *f;
+} ReaderF;
+
+int main(int argc, char **argv) {
+  if (argc == 2) {
+    if (readable(argv[1])) {
+      ReaderF *mainfile = (ReaderF *)malloc(sizeof(ReaderF));
+      if (mainfile == NULL)
+        return 1;
+      mainfile->f = fopen(argv[1], "r");
+      mainfile->c = 0;
+      mainfile->n = 0;
+      char *reader = malloc(BUFSIZE * sizeof(char));
+      if (reader == NULL)
+        return 1;
+      while (fgets(reader, BUFSIZE, mainfile->f) != NULL) {
+        mainfile->n++;
+        printf("%s", reader);
+      }
+      fclose(mainfile->f);
+      free(reader);
+      free(mainfile);
+      return 0;
+    } else {
+      perror("File does not exists");
+      return 1;
+    }
   }
-  return 0;
 }
--- compiler/lexical_analysis/c/run.sh
+++ compiler/lexical_analysis/c/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-gcc main.c lexer.c -o lexer
+gcc main.c -o lexer
 ./lexer testing.lua
 echo $?
+if [ "$1" == "--no-rm" ]; then
+    exit 0
+fi
 rm ./lexer

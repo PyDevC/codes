2f212e5dae43ff60b7a7756086feed1238cf512d
PyDevC <anuskcse@gmail.com>
2026-01-19 22:55:34 +0530

    c++: copy the string

--- c++/src/Main.cpp
+++ c++/src/Main.cpp
@@ -1,41 +1,27 @@
+#include <cstring>
 #include <iostream>
-#include <memory>
 
-class Entity {
-public:
-  int Age;
-  Entity() { std::cout << "Created" << std::endl; }
-  ~Entity() { std::cout << "Destroyed" << std::endl; }
-  void Print() { std::cout << "Print" << std::endl; }
-};
-
-class scopedPtr {
+class String {
 private:
-  Entity *m_ptr;
+  char *m_Buffer;
+  unsigned int m_Size;
 
 public:
-  scopedPtr(Entity *ptr) : m_ptr(ptr) {}
-  ~scopedPtr() { delete m_ptr; }
+  String(const char *string) {
+    m_Size = strlen(string);
+    m_Buffer = new char[m_Size + 1];
+    std::memcpy(m_Buffer, string, m_Size);
+  }
+  friend std::ostream &operator<<(std::ostream &stream, const String &string);
 };
+
+std::ostream &operator<<(std::ostream &stream, const String &string) {
+  stream << string.m_Buffer;
+  return stream;
+}
 
 int main(void) {
-  std::weak_ptr<Entity> entity6;
-  {
-    std::unique_ptr<Entity> entity = std::make_unique<Entity>();
-    std::shared_ptr<Entity> entity1 = std::make_shared<Entity>();
-    std::shared_ptr<Entity> entity2 = entity1;
-    std::shared_ptr<Entity> entity3 = entity1;
-    std::shared_ptr<Entity> entity4 = entity2;
-    std::shared_ptr<Entity> entity5 = entity1;
-    entity6 = entity1;
-    entity->Print();
-    entity1->Print();
-    entity2->Print();
-    entity3->Print();
-    entity4->Print();
-    entity5->Print();
-    entity6.lock()->Print();
-  }
-  std::cout << entity6.expired() << std::endl;
+  String string = "Test";
+  std::cout << string << std::endl;
   return 0;
 }

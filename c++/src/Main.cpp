fddc7a024b69d9252bcc3a25e20954c2debb740b
PyDevC <anuskcse@gmail.com>
2026-01-19 21:25:08 +0530

    c++: implicit type conversion

--- c++/src/Main.cpp
+++ c++/src/Main.cpp
@@ -5,23 +5,24 @@ using String = std::string;
 class Entity {
 private:
   String m_Name;
+  int m_Age;
 
 public:
   Entity() : m_Name("Unknown") {}
   Entity(const String &name) : m_Name(name) {}
+  Entity(int age) : m_Age(age) {}
   const String &GetName() const { return m_Name; }
+  int GetAge() const { return m_Age; }
 };
+
+void PrintEntity(const Entity &entity) {
+  std::cout << entity.GetName() << std::endl;
+  std::cout << entity.GetAge() << std::endl;
+}
 
 int main(void) {
   Entity e = Entity("Entity");
-  std::cout << e.GetName() << std::endl;
-
-  Entity *eh = new Entity[50]();
-  std::cout << eh->GetName() << std::endl;
-
-  Entity &reff = e;
-  std::cout << reff.GetName() << std::endl;
-
-  delete[] eh;
+  PrintEntity(e);
+  PrintEntity(11);
   return 0;
 }

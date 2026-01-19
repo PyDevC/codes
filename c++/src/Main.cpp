9725119ae12d77f0eb20af66826fbc691dd87754
PyDevC <anuskcse@gmail.com>
2026-01-19 23:13:21 +0530

    c++: sample string class

--- c++/src/Main.cpp
+++ c++/src/Main.cpp
@@ -12,6 +12,12 @@ public:
     m_Buffer = new char[m_Size + 1];
     std::memcpy(m_Buffer, string, m_Size);
   }
+  String(const String &other) : m_Size(other.m_Size) {
+    m_Buffer = new char[m_Size + 1];
+    std::memcpy(m_Buffer, other.m_Buffer, m_Size + 1);
+  }
+  ~String() { delete m_Buffer; }
+  char &operator[](const int index) { return m_Buffer[index]; }
   friend std::ostream &operator<<(std::ostream &stream, const String &string);
 };
 
@@ -21,7 +27,10 @@ std::ostream &operator<<(std::ostream &stream, const String &string) {
 }
 
 int main(void) {
-  String string = "Test";
+  String string = "Cherno";
+  String second = string;
+  second[4] = 'a';
   std::cout << string << std::endl;
+  std::cout << second << std::endl;
   return 0;
 }

b0ae3f559b4ee7c28cd973836178d3c15edb81c3
PyDevC <anuskcse@gmail.com>
2026-01-19 22:37:24 +0530

    c++: shared pointers and unique pointers

--- c++/src/Main.cpp
+++ c++/src/Main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <memory>
 
 class Entity {
 public:
   int Age;
   Entity() { std::cout << "Created" << std::endl; }
   ~Entity() { std::cout << "Destroyed" << std::endl; }
+  void Print() { std::cout << "Print" << std::endl; }
 };
 
 class scopedPtr {
@@ -18,8 +20,18 @@ public:
 
 int main(void) {
   {
-    scopedPtr s = new Entity();
-    scopedPtr e(new Entity());
+    std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+    std::shared_ptr<Entity> entity1 = std::make_shared<Entity>();
+    std::shared_ptr<Entity> entity2 = entity1;
+    std::shared_ptr<Entity> entity3 = entity1;
+    std::shared_ptr<Entity> entity4 = entity2;
+    std::shared_ptr<Entity> entity5 = entity1;
+    entity->Print();
+    entity1->Print();
+    entity2->Print();
+    entity3->Print();
+    entity4->Print();
+    entity5->Print();
   }
   return 0;
 }

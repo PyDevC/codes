30e005fda814b6f98de51402572dfd66e4a1fbce
PyDevC <anuskcse@gmail.com>
2026-01-19 20:31:49 +0530

    c++: heap and stack allocation in c++

--- c++/src/Main.cpp
+++ c++/src/Main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
-using namespace std;
+using String = std::string;
+
+class Entity {
+private:
+  String m_Name;
+
+public:
+  Entity() : m_Name("Unknown") {}
+  Entity(const String &name) : m_Name(name) {}
+  const String &GetName() const { return m_Name; }
+};
+
 int main(void) {
-  char name[5] = {'H', 'I', '!', '!', '\0'};
-  cout << name << endl;
+  Entity e = Entity("Entity");
+  std::cout << e.GetName() << std::endl;
+
+  Entity *eh = new Entity[50]();
+  std::cout << eh->GetName() << std::endl;
+
+  Entity &reff = e;
+  std::cout << reff.GetName() << std::endl;
+
+  delete[] eh;
   return 0;
 }

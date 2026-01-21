88b2984af990ee7c33f2fe98e3a6aaf049c8c9df
PyDevC <anuskcse@gmail.com>
2026-01-21 19:37:24 +0530

    c++: templates

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,7 +1,12 @@
 #include "../../Engine/src/Engine.h"
+template <typename T> void Print(T value) { std::cout << value << std::endl; }
 
 int main(void) {
   engine::Engine engine;
   engine.EchoVectorSpaceCoordinates();
+  Print<int>(10);
+  Print<std::string>("Game Engine");
+  Print<float>(4.4f);
+  Print<double>(4.234);
   return 0;
 }

3a683285e038cc8235da7c4c0cfe3f7b55ed2335
PyDevC <anuskcse@gmail.com>
2026-01-22 15:58:02 +0530

    c++: Standard Arrays

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,4 +1,5 @@
 #include "../../Engine/src/Engine.h"
+#include <array>
 
 using namespace engine;
 int main(void) {
@@ -6,5 +7,8 @@ int main(void) {
   eng.EchoVectorSpaceCoordinates();
   auto coord = eng.vs.GetVectorSpace();
   std::cout << coord << std::endl;
+  std::array<int, 5> data;
+  int size = data.size();
+  std::cout << size << std::endl;
   return 0;
 }

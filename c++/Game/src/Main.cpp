ed7d9a87f40dd2828415820a049a40d722eea754
PyDevC <anuskcse@gmail.com>
2026-01-25 20:09:59 +0530

    c++: unions in c++

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,19 +1,47 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
+
+struct Workers {
+  enum {
+    MAGE,
+    ARCHER,
+    RAWHAMMER,
+  } type;
+  union {
+    struct {
+      float power;
+      float short_range_power;
+      float long_range_power;
+      float health;
+      char *weapon;
+      char *first_skill;
+      char *power_skill;
+      char *general_skill;
+    } mage;
+
+    struct {
+      float power;
+      float power_shot_range;
+      float number_arrow;
+      float shooting_range;
+      char *first_skill;
+      char *power_skill;
+      char *general_skill;
+    } archer;
+
+    struct {
+      float power;
+      float meele_power;
+      float weapon_efficiency;
+      float number_arrow;
+      float shooting_range;
+      char *first_skill;
+      char *power_skill;
+      char *general_skill;
+    } raw_hammer;
+  } player;
+};
 
 int main(void) {
-  // sorting
-  std::vector<int> v = {1, 23, 234234, 234, 234, 234, 23};
-  std::sort(v.begin(), v.end(), [](int a, int b) {
-    if (a >= 100) {
-      return a > b;
-    } else {
-      return a < b;
-    }
-  });
-  for (int value : v) {
-    std::cout << value << std::endl;
-  }
+  Workers worker1 = {.type = Workers::MAGE, .player = {.mage = {}}};
   return 0;
 }

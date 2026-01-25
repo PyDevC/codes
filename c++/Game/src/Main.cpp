f6cdb177e4c71170776c0a1cd3d9c033e1ee0a99
PyDevC <anuskcse@gmail.com>
2026-01-25 16:15:15 +0530

    c++: getting timing of your code

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,13 +1,31 @@
-#include "../../Engine/src/Engine.h"
-#include <algorithm>
-#include <vector>
+#include <chrono>
+#include <iostream>
+#include <string>
 
-using namespace engine;
-using namespace std;
+struct Timer {
+public:
+  std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+  std::chrono::duration<float> duration;
+
+  Timer() { start = std::chrono::high_resolution_clock::now(); }
+  ~Timer() {
+    end = std::chrono::high_resolution_clock::now();
+    duration = end - start;
+    float ms = duration.count() * 1000.0f;
+    std::cout << ms << " ms";
+  }
+};
+
+void Function() {
+  Timer timer;
+  int a;
+  for (int i = 0; i < 1000000; i++) {
+    a = 10;
+  }
+  std::cout << a << std::endl;
+}
+
 int main(void) {
-  std::vector<int> values = {1, 2, 3, 4, 5};
-  auto it = std::find_if(values.begin(), values.end(),
-                         [](int value) { return value > 3; });
-  std::cout << *it << std::endl;
+  Function();
   return 0;
 }

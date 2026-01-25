ffd12b75d3662031d509ccc5f12011edd8bf7f06
PyDevC <anuskcse@gmail.com>
2026-01-25 14:23:04 +0530

    c++: lambda functions

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,5 +1,17 @@
 #include "../../Engine/src/Engine.h"
-#include <array>
+#include <functional>
+#include <vector>
+
+void Hello() { std::cout << "Hello" << std::endl; }
+
+void PrintValue(int value) { std::cout << value << std::endl; }
+
+void forevery(const std::vector<int> &values,
+              const std::function<void(int)> &func) {
+  for (int value : values) {
+    func(value);
+  }
+}
 
 using namespace engine;
 int main(void) {
@@ -7,8 +19,16 @@ int main(void) {
   eng.EchoVectorSpaceCoordinates();
   auto coord = eng.vs.GetVectorSpace();
   std::cout << coord << std::endl;
-  std::array<int, 5> data;
-  int size = data.size();
-  std::cout << size << std::endl;
+  int a = 5;
+
+  void (*hellofunction)(); // ok this is something amazing Can I use it in c
+  hellofunction = Hello;   // Syntax is type(*variablename)(parameter);
+  hellofunction();
+
+  auto function = Hello; // Simplistic way to write function pointer
+  function();
+  auto lambda = [=](int value) { std::cout << value + a << std::endl; };
+  std::vector<int> values = {1, 2, 3, 4, 5};
+  forevery(values, lambda);
   return 0;
 }

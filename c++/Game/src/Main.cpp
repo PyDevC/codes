3cc3ff718990c2b3718c667e9f48834f88072202
PyDevC <anuskcse@gmail.com>
2026-01-25 16:42:22 +0530

    c++: Multi-dimensional arrays

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,31 +1,22 @@
-#include <chrono>
-#include <iostream>
-#include <string>
-
-struct Timer {
-public:
-  std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-  std::chrono::duration<float> duration;
-
-  Timer() { start = std::chrono::high_resolution_clock::now(); }
-  ~Timer() {
-    end = std::chrono::high_resolution_clock::now();
-    duration = end - start;
-    float ms = duration.count() * 1000.0f;
-    std::cout << ms << " ms";
-  }
-};
-
-void Function() {
-  Timer timer;
-  int a;
-  for (int i = 0; i < 1000000; i++) {
-    a = 10;
-  }
-  std::cout << a << std::endl;
-}
+#include <stdlib.h>
 
 int main(void) {
-  Function();
+  int size = 2;
+  int ***array2 = new int **[size];
+  for (int i = 0; i < size; i++) {
+    array2[i] = new int *[size];
+    for (int j = 0; j < size; j++) {
+      array2[i][j] = new int[size];
+    }
+  }
+  delete[] array2;
+
+  int ***array3 = new int **[size];
+  for (int i = 0; i < size; i++) {
+    array3[i] = new int *[size];
+    for (int j = 0; j < size; j++) {
+      array3[i][j] = new int[size];
+    }
+  }
   return 0;
 }

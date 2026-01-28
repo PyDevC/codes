c3a6d1817b312d54b90047d4558c20424de51813
PyDevC <anuskcse@gmail.com>
2026-01-28 15:29:06 +0530

    added .gitignore and more string to assembly

--- assembly/hello.asm
+++ assembly/hello.asm
@@ -1,12 +1,19 @@
 section .data
-msg db 'Hello, World!\n', 0Ah
+msg db 'Hello, World!', 0Ah
+a db 'Anushk', 0Ah
 
 section .text
 global _start
 
 _start:
-    mov edx, 13
+    mov edx, 13 ;; Allocate 13 bytes to write Hello, World!
     mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 80h
+
+    mov edx, 6
+    mov ecx, a
     mov ebx, 1
     mov eax, 4
     int 80h

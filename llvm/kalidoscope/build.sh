76be5400ed1c101fd291110438e4b8f00fef90c0
PyDevC <anuskcse@gmail.com>
2026-02-10 12:17:23 -0500

    kalidoscope: set build to use clang compilers

--- llvm/kalidoscope/build.sh
+++ llvm/kalidoscope/build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ "$1" == "fmt" ]]; then
+    clang-format -i src/*.cpp
+fi
+
 mkdir -p build
 pushd build
-    cmake ..
+    cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
     make
     code=$(echo $?)
     if [[ "$code" -eq "0" ]]; then

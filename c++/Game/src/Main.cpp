cb3489c7d08abbc3b68fa88c37d4b665c8cb6699
PyDevC <anuskcse@gmail.com>
2026-03-13 12:06:41 +0000

    c++: using variants in c++

--- c++/Game/src/Main.cpp
+++ c++/Game/src/Main.cpp
@@ -1,26 +1,11 @@
-#include <fstream>
 #include <iostream>
-#include <optional>
-
-std::optional<std::string> ReadFile(const std::string &filepath) {
-  std::ifstream stream(filepath);
-  if (stream) {
-    std::string result;
-    stream.close();
-    return result;
-  }
-  return {};
-}
+#include <variant>
 
 int main(void) {
-  std::optional<std::string> data = ReadFile("data.txt");
-  std::string value = data.value_or("Not present");
-  std::optional<int> count;
-  int c = count.value_or(100);
-  if (data.has_value()) {
-    std::cout << "File read !!!\n";
-  } else {
-    std::cout << "Failure to read!!!\n";
-  }
+  std::variant<std::string, int> data;
+  data = "Cherno";
+  std::cout << std::get<std::string>(data) << "\n";
+  data = 10;
+  std::cout << std::get<int>(data) << "\n";
   return 0;
 }

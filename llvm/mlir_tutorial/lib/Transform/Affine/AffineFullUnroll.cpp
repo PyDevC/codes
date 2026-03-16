07728136649fb3f68c7a047ddbf3b9ae914abef4
PyDevC <anuskcse@gmail.com>
2026-03-16 15:38:41 +0000

    mlir_tutorial: added AffineFullUnroll using Patter rewrite engine

--- llvm/mlir_tutorial/lib/Transform/Affine/AffineFullUnroll.cpp
+++ llvm/mlir_tutorial/lib/Transform/Affine/AffineFullUnroll.cpp
@@ -1,9 +1,37 @@
 #include "lib/Transform/Affine/AffineFullUnroll.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 namespace mlir {
 namespace toy {
 
-void AffineFullUnrollPass::runOnOperation() { return; }
+void AffineFullUnrollPass::runOnOperation() {
+  getOperation().walk([&](affine::AffineForOp op) {
+    if (llvm::failed(affine::loopUnrollFull(op))) {
+      op.emitError("loop unrolling failed");
+      signalPassFailure();
+    }
+  });
+  return;
+}
+
+struct AffineFullUnrollPattern : public OpRewritePattern<affine::AffineForOp> {
+  AffineFullUnrollPattern(mlir::MLIRContext *context)
+      : OpRewritePattern<affine::AffineForOp>(context, 1) {}
+  LogicalResult matchAndRewrite(affine::AffineForOp op,
+                                PatternRewriter &rewriter) const override {
+    return affine::loopUnrollFull(op);
+  }
+};
+
+void AffineFullUnrollPassAsPatternRewrite::runOnOperation() {
+    mlir::RewritePatternSet patterns(&getContext());
+    patterns.add<AffineFullUnrollPattern>(&getContext());
+    (void)applyPatternsGreedily(getOperation(), std::move(patterns));
+}
 
 } // namespace toy
 } // namespace mlir
--- llvm/mlir_tutorial/lib/Transform/Affine/AffineFullUnroll.h
+++ llvm/mlir_tutorial/lib/Transform/Affine/AffineFullUnroll.h
@@ -1,9 +1,9 @@
 #ifndef LIB_TRANSFORM_AFFINE_AFFINEFULLUNROLL_H_
 #define LIB_TRANSFORM_AFFINE_AFFINEFULLUNROLL_H_
 
-#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/include/mlir/Pass/Pass.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Pass/Pass.h"
 
 namespace mlir {
 namespace toy {
@@ -13,9 +13,22 @@ class AffineFullUnrollPass
                          OperationPass<mlir::func::FuncOp>> {
 private:
   void runOnOperation() override;
-  llvm::StringRef getArgument() const final { return "affine-full-unroll"; }
-  llvm::StringRef getDescription() const final {
+  mlir::StringRef getArgument() const final { return "affine-full-unroll"; }
+  mlir::StringRef getDescription() const final {
     return "Fully unroll all affine loops";
+  }
+};
+
+class AffineFullUnrollPassAsPatternRewrite
+    : public PassWrapper<AffineFullUnrollPassAsPatternRewrite,
+                         OperationPass<mlir::func::FuncOp>> {
+private:
+  void runOnOperation() override;
+  mlir::StringRef getArgument() const final {
+    return "affine-full-unroll-rewrite";
+  }
+  mlir::StringRef getDescription() const final {
+    return "Fully unroll all affine loops using patttern rewrite engine";
   }
 };
 

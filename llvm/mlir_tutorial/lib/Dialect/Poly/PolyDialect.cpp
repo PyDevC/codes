2bafd5c593153380e5d702a3f72b095ed4e9aecb
PyDevC <anuskcse@gmail.com>
2026-03-24 12:20:29 +0000

    mlir_tutorial: PolyDialect `empty dialect` setup with tablegen

---
+++ llvm/mlir_tutorial/lib/Dialect/Poly/PolyDialect.cpp
@@ -0,0 +1,12 @@
+#include "lib/Dialect/Poly/PolyDialect.h"
+#include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+
+namespace mlir {
+namespace toy {
+namespace poly {
+
+void PolyDialect::initialize(){}
+
+} // namespace poly
+} // namespace toy
+} // namespace mlir

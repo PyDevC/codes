d75be1661c35da258b95b5ae421ef70d2bca1c16
PyDevC <anuskcse@gmail.com>
2026-03-30 14:37:56 +0000

    mlir_tutorial: added materializer

--- llvm/mlir_tutorial/lib/Dialect/Poly/PolyDialect.cpp
+++ llvm/mlir_tutorial/lib/Dialect/Poly/PolyDialect.cpp
@@ -2,6 +2,7 @@
 
 #include "lib/Dialect/Poly/PolyOps.h"
 #include "lib/Dialect/Poly/PolyType.h"
+#include "mlir/Dialect/Transform/Interfaces/TransformInterfaces.h"
 #include "mlir/IR/Builders.h"
 #include "llvm/ADT/TypeSwitch.h"
 
@@ -27,6 +28,15 @@ void PolyDialect::initialize() {
   addOperations<
 #include "lib/Dialect/Poly/PolyOps.cpp.inc"
       >();
+}
+
+Operation *PolyDialect::materializeConstant(OpBuilder &builder, Attribute value,
+                                            Type type, Location loc) {
+  auto coeffs = llvm::dyn_cast<DenseIntElementsAttr>(value);
+  if (!coeffs) {
+    return nullptr;
+  }
+  return builder.create<ConstantOp>(loc, type, coeffs);
 }
 
 } // namespace poly
